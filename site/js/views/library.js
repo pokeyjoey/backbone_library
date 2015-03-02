218c1a9034f572beb7ffb01f49e63681637249c3
Jeff Roche <jkeauctions@gmail.com>
2015-03-02 07:03:51 -0500

    REST backbone app

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -9,10 +9,13 @@ app.LibraryView = Backbone.View.extend({
         'click #add':'addBook'
     },
 
-    initialize: function( initialBooks ) {
+    initialize: function() {
         this.collection = new app.Library( initialBooks );
-        this.listenTo( this.collection, 'add', this.renderBook );
+        this.collection.fetch(reset: true});
         this.render();
+
+        this.listenTo( this.collection, 'add', this.renderBook );
+        this.listenTo( this.collection, 'reset', this.render );
     },
 
     // render library by rendering each book in its collection.
